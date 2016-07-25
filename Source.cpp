56b3a34119cf823fa105d948a06419bef7cbbaa3
yuni-net <yuni.net.liberty@gmail.com>
2016-07-25 12:01:28 +0900

    first

---
+++ Source.cpp
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+
+// list of standard io
+// scanf
+// std::cin
+// std::cin.getline
+// gets
+
+
+int main()
+{
+	static const int capacity = 256;
+	char buffer[capacity];
+	std::string container;
+
+	/*
+	std::cout << ">";
+	scanf(" %s", buffer);
+	std::cout << std::endl;
+	std::cout << "scanf(" << buffer << ")" << std::endl;
+	std::cout << "stdin to clear" << std::endl;
+	std::cin >> container;
+	std::cout << container << std::endl;
+	std::cout << std::endl;
+
+	std::cout << ">";
+	std::cin >> container;
+	std::cout << std::endl;
+	std::cout << "std::cin(" << container << ")" << std::endl;
+	std::cout << "stdin to clear" << std::endl;
+	std::cin >> container;
+	std::cout << container << std::endl;
+	std::cout << std::endl;
+
+	std::cout << ">";
+	std::cin.getline(buffer, capacity);
+	std::cout << std::endl;
+	std::cout << "std::cin.getline(" << buffer << ")" << std::endl;
+	std::cout << "stdin to clear" << std::endl;
+	std::cin >> container;
+	std::cout << container << std::endl;
+	std::cout << std::endl;
+
+	std::cout << ">";
+	gets(buffer);
+	std::cout << std::endl;
+	std::cout << "gets(" << buffer << ")" << std::endl;
+	std::cout << "stdin to clear" << std::endl;
+	std::cin >> container;
+	std::cout << container << std::endl;
+	std::cout << std::endl;
+	//*/
+
+	std::cout << ">";
+	std::getline(std::cin, container);
+	std::cout << "std::getline(" << container << ")" << std::endl;
+	std::cout << "stdin to clear" << std::endl;
+	std::cin >> container;
+	std::cout << container << std::endl;
+	std::cout << std::endl;
+
+	return 0;
+}
